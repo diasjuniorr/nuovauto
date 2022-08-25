be3eed67731247660f6c4aaf5162e7d365caab21
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-08-25 16:14:57 +0200

    name all dialogs

--- src/components/pericia/pericia-img/pericia-img.component.tsx
+++ src/components/pericia/pericia-img/pericia-img.component.tsx
@@ -4,20 +4,20 @@ import Dialog from "../pericia-img-dialog/dialog.component";
 const PericiaImg = () => {
   return (
     <PericiaImgContainer>
-      <Dialog top={100} left={340} />
-      <Dialog top={100} left={540} />
-      <Dialog top={100} left={730} />
-      <Dialog top={100} left={900} />
+      <Dialog top={100} left={340} name="parafango-ad" />
+      <Dialog top={100} left={540} name="porta-ad" />
+      <Dialog top={100} left={730} name="porta-pd" />
+      <Dialog top={100} left={900} name="parafango-pd" />
       <Dialog top={370} left={270} name="cofano" />
       <Dialog top={370} left={670} name="tetto" />
-      <Dialog top={260} left={780} />
-      <Dialog top={480} left={780} />
-      <Dialog top={370} left={940} />
-      <Dialog top={370} left={1067} />
-      <Dialog top={650} left={340} />
-      <Dialog top={650} left={540} />
-      <Dialog top={650} left={730} />
-      <Dialog top={650} left={900} />
+      <Dialog top={260} left={780} name="pianzone-d" />
+      <Dialog top={480} left={780} name="pianzone-s" />
+      <Dialog top={370} left={940} name="sportello-s" />
+      <Dialog top={370} left={1067} name="sportello-i" />
+      <Dialog top={650} left={340} name="parafango-as" />
+      <Dialog top={650} left={540} name="porta-as" />
+      <Dialog top={650} left={730} name="porta-ps" />
+      <Dialog top={650} left={900} name="parafango-ps" />
     </PericiaImgContainer>
   );
 };
