7ea3576a7000d5fc65af224dd7221973c5aec0d7
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-08-25 16:49:50 +0200

    add car part interface

---
+++ src/shared/interfaces/car-part.interface.ts
@@ -0,0 +1,9 @@
+export interface CarPart {
+  name: string;
+  isAluminum: boolean;
+  shouldPaint: boolean;
+  shouldReplace: boolean;
+  shouldGlue: boolean;
+  smallSmash: number | string;
+  smash: number | string;
+}
