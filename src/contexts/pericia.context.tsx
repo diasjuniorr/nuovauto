56dfc49d87b4b4f5b171dca9ca28d8da21ad0a96
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-08-26 10:39:33 +0200

    refact update car part to add notes

--- src/contexts/pericia.context.tsx
+++ src/contexts/pericia.context.tsx
@@ -24,6 +24,7 @@ export const PericiaProvider: React.FC<Props> = ({ children }) => {
       const index = prev.findIndex((cp) => cp.name === carPart.name);
       const newCarParts = [...prev];
       newCarParts[index] = carPart;
+      newCarParts[index].note = generateNotes(carPart);
       return newCarParts;
     });
   };
@@ -33,4 +34,59 @@ export const PericiaProvider: React.FC<Props> = ({ children }) => {
       {children}
     </PericiaContext.Provider>
   );
+};
+
+const generateNotes = (carPart: CarPart) => {
+  const {
+    isAluminum,
+    shouldPaint,
+    shouldReplace,
+    shouldGlue,
+    smallSmash,
+    smash,
+  } = carPart;
+
+  let smashNotes = "";
+  let smallSmashNotes = "";
+  let paintNotes = "";
+  let isAluminumNotes = "";
+  let shouldReplaceNotes = "";
+  let shouldGlueNotes = "";
+
+  if (+smallSmash >= 610 || +smash >= 610 || +smallSmash + +smash >= 610) {
+    smashNotes = "max";
+  } else {
+    smallSmashNotes = smallSmash ? String(smallSmash) : "";
+    smashNotes = smash ? `>${String(smash)}` : "";
+  }
+
+  if (shouldPaint) {
+    paintNotes = "p";
+  }
+
+  if (isAluminum) {
+    isAluminumNotes = "al";
+  }
+
+  if (shouldReplace) {
+    shouldReplaceNotes = "x";
+  }
+
+  if (shouldGlue) {
+    shouldGlueNotes = "c";
+  }
+
+  if (
+    !smashNotes &&
+    !smallSmashNotes &&
+    !paintNotes &&
+    !isAluminumNotes &&
+    !shouldReplaceNotes &&
+    !shouldGlueNotes
+  ) {
+    return "0";
+  }
+
+  return `${smallSmashNotes} ${smashNotes} ${paintNotes} ${isAluminumNotes} ${shouldReplaceNotes} ${shouldGlueNotes}
+      `;
 };
