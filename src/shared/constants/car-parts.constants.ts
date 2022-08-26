f3aef02d72b528a7e426610ad0cc0230f16e8cbc
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-08-26 12:39:45 +0200

    add car parts canvas coordinates

--- src/shared/constants/car-parts.constants.ts
+++ src/shared/constants/car-parts.constants.ts
@@ -29,3 +29,20 @@ export const CAR_PARTS_LIST: CarPart[] = Object.keys(CAR_PARTS).map((key) => {
     note: "0",
   };
 });
+
+export const CAR_PARTS_CANVAS_COORDINATES = {
+  "Parafango-ad": { x: 330, y: 155 },
+  "Porta-ad": { x: 510, y: 155 },
+  "Porta-pd": { x: 689, y: 155 },
+  "Parafango-pd": { x: 860, y: 155 },
+  Cofano: { x: 220, y: 410 },
+  Tetto: { x: 660, y: 410 },
+  "Pianzone-d": { x: 800, y: 300 },
+  "Pianzone-s": { x: 800, y: 505 },
+  "Sportello-s": { x: 960, y: 410 },
+  "Sportello-i": { x: 1130, y: 410 },
+  "Parafango-as": { x: 330, y: 665 },
+  "Porta-as": { x: 510, y: 655 },
+  "Porta-ps": { x: 689, y: 655 },
+  "Parafango-ps": { x: 860, y: 655 },
+};
