0b471e68529c0d90217adec3cf5a3f999e8d79c9
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-08-31 11:56:31 +0200

    add pericia form

--- src/routes/pericia/pericia.component.tsx
+++ src/routes/pericia/pericia.component.tsx
@@ -1,12 +1,90 @@
 import PericiaImg from "../../components/pericia/pericia-img/pericia-img.component";
 import PDFGenerator from "../../components/pdf/pdf.component";
+import { Box, Container, Grid, TextField, Typography } from "@mui/material";
+import Button from "@mui/material/Button";
 
 const Pericia = () => {
   return (
-    <>
+    <Container component="main" maxWidth="xl">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Pericia
+        </Typography>
+        <Box component="form" noValidate sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={12}>
+              <TextField
+                name="cliente"
+                required
+                fullWidth
+                id="costumerName"
+                label="Cliente"
+                autoFocus
+                variant="standard"
+              />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <TextField
+                required
+                fullWidth
+                id="placa"
+                label="Placa"
+                name="placa"
+                variant="standard"
+              />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <TextField
+                required
+                fullWidth
+                id="brand"
+                label="Marca"
+                name="brand"
+                variant="standard"
+              />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <TextField
+                required
+                fullWidth
+                name="model"
+                label="Modelo"
+                id="model"
+                variant="standard"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="date"
+                label="Data"
+                id="date"
+                variant="standard"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign Up
+          </Button>
+          <Grid container justifyContent="flex-end"></Grid>
+        </Box>
+      </Box>
       <PericiaImg />
       <PDFGenerator />
-    </>
+    </Container>
   );
 };
 
