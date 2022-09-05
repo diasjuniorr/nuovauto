efd8cee3de0644b6e8271ed26a2b029f7ab36e6a
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-09-05 23:30:10 +0200

    add sticky footer

--- src/routes/navigation/navigation.component.tsx
+++ src/routes/navigation/navigation.component.tsx
@@ -5,6 +5,8 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
 
 const Navigation = () => {
   return (
@@ -28,6 +30,34 @@ const Navigation = () => {
       </Box>
 
       <Outlet />
+
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "15vh",
+        }}
+      >
+        <CssBaseline />
+        <Box
+          component="footer"
+          sx={{
+            py: 3,
+            px: 2,
+            mt: "auto",
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? theme.palette.grey[200]
+                : theme.palette.grey[800],
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography variant="body1" textAlign="center">
+              Nuovauto © 2022
+            </Typography>
+          </Container>
+        </Box>
+      </Box>
     </>
   );
 };
