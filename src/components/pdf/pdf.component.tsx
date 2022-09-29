86ca3ace584b9734358c55fcae8b8e8e73f8b247
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-09-29 22:46:30 +0200

    refact pdf component

--- src/components/pdf/pdf.component.tsx
+++ src/components/pdf/pdf.component.tsx
@@ -7,6 +7,9 @@ import {
 import Canvas from "./canvas.component";
 import { CAR_PARTS_CANVAS_COORDINATES } from "../../shared/constants/car-parts.constants";
 import { Button } from "@mui/material";
+import { CarPart } from "../../shared/interfaces/car-part.interface";
+import { Car, Costumer } from "../../shared/interfaces/pericia.interface";
+import { date } from "yup/lib/locale";
 
 const carroImg = require("../../assets/pericia.jpg");
 
@@ -29,37 +32,10 @@ const PDFGenerator: React.FC = () => {
       //draw image
       // context.drawImage(img, 0, 30, 1200, 800);
       context.drawImage(img, 20, 80, 1150, 1100);
-      context.font = "26px Arial";
-      context.fillStyle = "blue";
 
-      //draw car part notes
-      carParts.forEach((part) => {
-        const { x, y } =
-          CAR_PARTS_CANVAS_COORDINATES[
-            part.name as keyof typeof CAR_PARTS_CANVAS_COORDINATES
-          ];
-        context.fillText(part.note.smashes, x, y);
-      });
-
-      //draw identification
-      context.font = "26px Arial";
-      context.fillStyle = "black";
-      context.fillText(`Cliente: ${CostumerName}`, 10, 40);
-      context.fillText(`Marca: ${brand}`, 600, 40);
-      context.fillText(`Modello: ${model}`, 600, 70);
-      context.fillText(`Targa: ${plate}`, 980, 40);
-      context.fillText(`Data: ${date.toLocaleDateString("pt-br")}`, 980, 70);
-      if (finished) {
-        context.fillStyle = "red";
-        context.fillText(`Liquidata`, 10, 70);
-      }
-
-      //draw border
-      context.beginPath();
-      context.lineWidth = 2;
-      context.strokeStyle = "black";
-      context.rect(0, 0, canvas.width, canvas.height);
-      context.stroke();
+      drawCarParts(context, carParts);
+      drawIdentification(context, costumer, car, finished, date);
+      drawBorder(context, canvas.width, canvas.height);
     };
   };
 
@@ -87,6 +63,50 @@ const PDFGenerator: React.FC = () => {
     </>
   );
 };
+
+function drawCarParts(context: any, carParts: CarPart[]) {
+  context.font = "26px Arial";
+  context.fillStyle = "blue";
+
+  carParts.forEach((part) => {
+    const { x, y } =
+      CAR_PARTS_CANVAS_COORDINATES[
+        part.name as keyof typeof CAR_PARTS_CANVAS_COORDINATES
+      ];
+    context.fillText(part.note.smashes, x, y);
+  });
+}
+
+function drawIdentification(
+  context: any,
+  costuner: Costumer,
+  car: Car,
+  finished: boolean,
+  date: Date
+) {
+  const { brand, model, plate } = car;
+  const { name } = costuner;
+
+  context.font = "26px Arial";
+  context.fillStyle = "black";
+  context.fillText(`Cliente: ${name}`, 10, 40);
+  context.fillText(`Marca: ${brand}`, 600, 40);
+  context.fillText(`Modello: ${model}`, 600, 70);
+  context.fillText(`Targa: ${plate}`, 980, 40);
+  context.fillText(`Data: ${date.toLocaleDateString("pt-br")}`, 980, 70);
+  if (finished) {
+    context.fillStyle = "red";
+    context.fillText(`Liquidata`, 10, 70);
+  }
+}
+
+function drawBorder(context: any, width: number, height: number) {
+  context.beginPath();
+  context.lineWidth = 2;
+  context.strokeStyle = "black";
+  context.rect(0, 0, width, height);
+  context.stroke();
+}
 
 const removeWhiteSpaces = (str: string) => {
   return str.replace(/\s/g, "_");
