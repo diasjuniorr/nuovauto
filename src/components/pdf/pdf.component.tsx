5f69f3b9a44148003c62115b4ac75b1d90514856
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-10-18 23:34:21 +0200

    add pdf legend

--- src/components/pdf/pdf.component.tsx
+++ src/components/pdf/pdf.component.tsx
@@ -149,8 +149,8 @@ function drawIdentification(context: any, pdfInfoObject: PDFInfoObject) {
 function drawPDFLegend(context: any) {
   context.font = "26px Arial";
   context.fillStyle = "black";
-  context.fillText(`V: Verniciare`, 10, 1100);
-  context.fillText(`C: Cola`, 10, 1130);
+  context.fillText(`C: Cola`, 10, 1100);
+  context.fillText(`V: Verniciare`, 10, 1130);
   context.fillText(`AL: Alumnino`, 10, 1160);
   context.fillText(`SOST: Sostituire`, 10, 1190);
 }
