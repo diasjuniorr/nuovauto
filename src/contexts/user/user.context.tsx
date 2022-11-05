eeb67962c1dc19568073742d09f7a45faffa84b9
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-11-05 18:37:48 +0100

    add user context

---
+++ src/contexts/user/user.context.tsx
@@ -0,0 +1,65 @@
+import { createContext, useEffect, useReducer } from "react";
+import supabase from "../../utils/supabase/supabase.utils";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+interface User {
+  id: string;
+  email: string;
+  name: string;
+  token: string;
+}
+
+enum UserActionTypes {
+  SET_USER = "SET_USER",
+}
+
+interface UserReducerAction {
+  type: UserActionTypes;
+  payload: User | null;
+}
+
+interface UserState {
+  user: User | null;
+}
+
+const userReducer = (state: UserState, action: UserReducerAction) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case UserActionTypes.SET_USER:
+      return { ...state, user: payload };
+    default:
+      return state;
+  }
+};
+
+interface UserContextProps {
+  user: User | null;
+  setUser: (user: User | null) => void;
+}
+
+export const UserContext = createContext<UserContextProps>({
+  user: null,
+  setUser: () => {},
+});
+
+export const UserProvider: React.FC<Props> = ({ children }) => {
+  const [{ user }, dispatch] = useReducer(userReducer, { user: null });
+
+  const setUser = (user: User | null) => {
+    dispatch({ type: UserActionTypes.SET_USER, payload: user });
+  };
+
+  const value = { user, setUser };
+
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((event, session) => {
+      if (event === "SIGNED_IN") console.log("debug: signed in", session);
+    });
+  }, []);
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
