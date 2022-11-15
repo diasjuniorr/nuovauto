ecb7e91ad64c6a073d3615f54a8c28891c395cc8
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-11-15 12:15:19 +0100

    chore: make sign up page unprotected

--- src/routes/auth/auth.component.tsx
+++ src/routes/auth/auth.component.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import SignIn from "./sign-in/sign-in.component";
+import SignUP from "./sign-up/signUp.component";
 
 const Auth = () => {
   return (
     <Routes>
       <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUP />} />
     </Routes>
   );
 };
--- src/routes/users/users.component.tsx
+++ src/routes/users/users.component.tsx
@@ -1,11 +1,9 @@
 import { Route, Routes } from "react-router-dom";
-import SignUP from "../auth/sign-up/signUp.component";
 import AddUser from "./add-users/add-users.component";
 
 const Users = () => {
   return (
     <Routes>
-      <Route path="/sign-up" element={<SignUP />} />
       <Route path="/add" element={<AddUser />} />
     </Routes>
   );
